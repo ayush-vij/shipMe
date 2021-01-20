ddc8a02789845a8d0c4dbcdd1c988c8f83e52ff2
Sadan Iqbal <sadaniqbal@gmail.com>
2021-01-20 20:06:04 +0500

    Added screen orientation lock to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private screenOrientation: ScreenOrientation
   ) {
     this.initializeApp();
   }
@@ -23,5 +26,10 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    // get current screen orientation
+    console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
+
+    // lock to portait screen orientation
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
   }
 }
