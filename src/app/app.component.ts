f99cfa0f0f74290b77fcf8a35a51e9193176ed4a
Sadan Iqbal <sadaniqbal@gmail.com>
2021-01-21 23:14:21 +0500

    Added splashScreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
+import { Platform } from "@ionic/angular";
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
-
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { ScreenOrientation } from "@ionic-native/screen-orientation/ngx";
+// import { timer } from "rxjs/observable/timer";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
+
+
+
 export class AppComponent {
+ 
+  tabBarElement: any;
+  splash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private screenOrientation: ScreenOrientation
+    // private screenOrientation: ScreenOrientation
   ) {
     this.initializeApp();
   }
@@ -25,11 +32,17 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      setTimeout(() => {
+        this.splash = false;
+      }, 4000);
     });
-    // get current screen orientation
-    console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
+    // // get current screen orientation
+    // console.log(this.screenOrientation.type); // logs the current orientation, example: 'landscape'
 
-    // lock to portait screen orientation
-    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+    // // lock to portait screen orientation
+    // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+
+    // // // allow user rotate
+    // // this.screenOrientation.lock();
   }
 }
