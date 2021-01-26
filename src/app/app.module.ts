2ad7cb307de888f2bafcbde617cf6d04a817947a
Sadan Iqbal <sadaniqbal@gmail.com>
2021-01-26 22:30:08 +0500

    Added super tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { SuperTabsModule} from "@ionic-super-tabs/angular"
 //import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -15,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule, AppRoutingModule, SuperTabsModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
