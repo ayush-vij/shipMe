b55d4505d3cc9940330e6b2a348582d8ae84c9b7
ayush-vij <ayushvij@live.in>
2021-04-25 17:25:43 -0700

    Added toastnotifs and fixes to reg

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 // import { AuthService } from '../auth.service';
-import { User } from '../auth.model';
+import { User } from '../dauth.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,6 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 // import { AuthService } from '../auth.service';
-import { User } from '../auth.model';
+import { User } from '../dauth.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
@@ -14,6 +14,8 @@ import { StatusBar } from "@ionic-native/status-bar/ngx";
 import { AuthService } from '../service/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-register',
@@ -24,8 +26,10 @@ export class RegisterPage {
   // form: FormGroup;
   tabBarElement: any;
   splash = true;
+  fname: string;
   email: string;
   password: string;
+  repassword: string;
 
   public appPages = [
     {
@@ -47,10 +51,22 @@ export class RegisterPage {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    public authService: AuthService
+    public authService: AuthService,
+    public toastController: ToastController
   ) { }
+
+  pwcheck(){
+    if (this.password == this.repassword){
+      this.signup();
+    }
+    else{
+      this.resetpw();
+      this.pwerror();
+    }
+  }
     
   signup() {
+    //this.authService.signup(this.fname, this.email, this.password, this.repassword);
     this.authService.signup(this.email, this.password);
     this.email = this.password = '';
   }
@@ -65,6 +81,19 @@ export class RegisterPage {
   
   ionViewWillEnter() {
       this.firebaseAuth.signOut();
+  }
+
+  async pwerror() {
+    const toast = await this.toastController.create({
+      message: 'The passwords do not match, please try again!',
+      duration: 4000
+    });
+    toast.present();
+  }
+
+  resetpw(){
+    this.password = null;
+    this.repassword = null;
   }
 
   NaviateToLogIn() {
