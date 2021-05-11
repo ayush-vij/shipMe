5b95dc1a033189d2b1ea74e87c398ac462dd718a
Sadan Iqbal <sadaniqbal@gmail.com>
2021-05-11 12:01:23 +0500

    Added SMS AUTHENTICATION  (DANI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,16 @@ import { environment } from 'src/environments/environment';
 import * as firebase from 'firebase';
 import { Firebase  } from '@ionic-native/firebase';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCRqbNvvvm-L3jfTHh8WmcJCbyneh7yHMo",
+    authDomain: "postship-2c320.firebaseapp.com",
+    databaseURL: "https://postship-2c320-default-rtdb.firebaseio.com",
+    projectId: "postship-2c320",
+    storageBucket: "postship-2c320.appspot.com",
+    messagingSenderId: "616220357262",
+    appId: "1:616220357262:web:d6c1e4eb8159903afa0fdf",
+    measurementId: "G-LD2YLVPGQX"
+})
 
 @NgModule({
   declarations: [AppComponent],
